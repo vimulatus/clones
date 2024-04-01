ddff9209e962b541d891ee5d708b364403889ff9
Vimulatus <vasu.mit2501@gmail.com>
2024-04-01 17:00:44 +0530

    feat(shared-ui): add storybook and compodoc

---
+++ libs/shared/ui/tailwind.config.js
@@ -0,0 +1,11 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('../../theme/src/tailwind.preset'), require('@spartan-ng/ui-core/hlm-tailwind-preset')],
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+};
